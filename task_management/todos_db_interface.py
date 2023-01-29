05d38a4ddacb30a46b85b7dbe07dc48f161983b1
bruno <bruno.valverde.95@outlook.es>
2023-01-29 17:12:01 +0100

    database for todos added

--- task_management/todos_db_interface.py
+++ task_management/todos_db_interface.py
@@ -7,33 +7,41 @@ Base = declarative_base()
 
 class TodoNode(Base):
     __tablename__ = "todos"
+    # values in the primary_key column must be unique for each row in the table, and you cannot insert a row without providing a value for that column
     id = Column(Integer, primary_key=True)
     parent_id = Column(Integer, nullable=True)
-    name = Column(String)
+    description_text = Column(String)
 
 
 class TodosManager:
-
+    # TODO: add integrate Manager object with creation of TODOs in user interface
     def __init__(self):
         self._engine = create_engine("sqlite:///todos_hierarchy.db")
         Base.metadata.create_all(self._engine)
         Session = sessionmaker(bind=self._engine)
         self._session = Session()
         # Add root node to the hierarchy
-        self._root = TodoNode(name="root")
+        self._root = TodoNode(description_text="root")
         self._session.add(self._root)
+        self._session.commit()
 
     def add_node2hierarchy(self, todo_text: str, parent: TodoNode = None):
         # Add a child node
         if parent is not None:
-            child = TodoNode(name=todo_text, parent_id=parent.id)
+            child = TodoNode(description_text=todo_text, parent_id=parent.id)
         else:
             # if parent is not specified use root node
-            child = TodoNode(name=todo_text, parent_id=self._root.id)
+            child = TodoNode(description_text=todo_text, parent_id=self._root.id)
         self._session.add(child)
-
-    def commit_session(self):
         self._session.commit()
+
+    def get_todo_by_text(self, todo_txt: str):
+        todo = self._session.query(TodoNode).filter_by(description_text=todo_txt).first()
+        return todo
+
+    def get_todo_by_id(self, todo_txt: str):
+        todo = self._session.query(TodoNode).filter_by(id=todo_txt).first()
+        return todo
 
     def query_all_nodes_in_hierarchy(self):
         # Query for all nodes in the hierarchy
@@ -45,4 +53,6 @@ class TodosManager:
 
 if __name__ == "__main__":
     manager = TodosManager()
-    manager.add_node2hierarchy("Go to the toilet")
+    todo_txt = "Go to the toilet"
+    manager.add_node2hierarchy(todo_txt)
+    manager.get_todo_by_text(todo_txt)
